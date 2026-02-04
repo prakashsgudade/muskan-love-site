52a33739b9778732c6c21bc35b8fb46271187992
prakashsgudade <prakashsgudade@gmail.com>
2026-02-04 22:49:10 +0530

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,12 +1,37 @@
 const text = document.getElementById("text");
 const yesBtn = document.getElementById("yesBtn");
 const noBtn = document.getElementById("noBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+// UNFOLD TEXTS
+const messages = [
+  "Thank you for saying Yes 💖",
+  "Yeh website kisi proposal ke liye nahi hai…",
+  "Bas ek honest feeling hai jo chup nahi rahi.",
+  "Tumhari khushi meri expectation se zyada important hai.",
+  "Agar kabhi tumhara mann kare… main yahin hoon."
+];
+
+let step = 0;
 
 // YES CLICK
 yesBtn.addEventListener("click", () => {
-  text.innerText = "Thank you for saying Yes 💖";
   yesBtn.style.display = "none";
   noBtn.style.display = "none";
+
+  text.innerText = messages[step];
+  nextBtn.classList.remove("hidden");
+});
+
+// NEXT CLICK
+nextBtn.addEventListener("click", () => {
+  step++;
+
+  if (step < messages.length) {
+    text.innerText = messages[step];
+  } else {
+    nextBtn.style.display = "none";
+  }
 });
 
 // NO BUTTON PRANK
